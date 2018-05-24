58587264ce30cb696870ac2f0c89e4ebf5143c54
Jacob Salzberg <jssalzbe@ncsu.edu>
2018-05-24 12:46:00 -0400

    Switch over to Java

---
+++ src/main/java/Srv.java
@@ -0,0 +1,13 @@
+/**
+ * The main class of the program.
+ * @author Jacob Salzberg
+ */
+public class Srv {
+    /**
+     * The entry point of the program.
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/srvTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Test the project.
+ * @author Jacob Salzberg
+ */
+public class srvTest {
+    @Test
+    public void test() {
+    }
+}
