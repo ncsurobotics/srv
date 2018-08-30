6c7f2cb196e5ff23f8555f3260207d1c1c7a91fe
Jacob Salzberg <jssalzbe@ncsu.edu>
2018-08-29 20:56:00 -0400

    Starting svr_cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <sys/sockets.h>
+
+auto main(int argv, char** argc) -> int {
+  return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+auto main() -> int {
+  try {
+    boost::asio::io_service io_service;
+    udp::socket socket(io_service, udp::endpoint(udp::v4(), 13));
+
+    while (true) {
+      boost::array<char, 1> recv_buf;
+      udp::endpoint remote_endpoint;
+      boost::system::error_code error;
+      socket.receive_from(boost::asio::buffer(recv_buf),
+                          remote_endpoint, 0, error);
+      if (error && error != boost::asio::error::message_size) {
+        throw boost::system::system_error(error);
+      }
+    }
+
+    // std::string message = "asdf";
+    // boost::system::error_code ignored_error;
+    // socket.send_to(boost::asio::buffer(message),
+    //                remote_endpoint, 0, ignored_error);
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+  std::cout << "Hi" << std::endl;
+  return 0;
+}
