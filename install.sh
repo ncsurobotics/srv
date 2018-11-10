af4dab6d8a0f41892142bd27313bba5da99044c3
Ben <bjfisher@ncsu.edu>
2018-11-10 12:24:05 -0500

    Added in global commands to start, watch, and start cams for SRV.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 sudo mkdir /usr/local/lib/python2.7/dist-packages/srv
 sudo cp -r . /usr/local/lib/python2.7/dist-packages/srv
 sudo rm -f /usr/local/lib/python2.7/dist-packages/srv/*.sh
+
+sudo cp srvStartCams.sh ~/bin/srvStartCams
+sudo cp srvStartServer.sh ~/bin/srvStartServer
+sudo cp srvWatch.sh ~/bin/srvWatch
+
+sudo chmod +x ~/bin/srvStartCams
+sudo chmod +x ~/bin/srvStartServer
+sudo chmod +x ~/bin/srvWatch
--- startSrv.sh
+++
@@ -1,1 +0,0 @@
-python startServer.py
