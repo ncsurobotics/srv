355a4430c339b727e2fc0ec19f1c04072d4a8fcd
Ben <bjfisher@ncsu.edu>
2018-11-10 11:49:15 -0500

    Added in ability to select different sources to watch. More error handling.

--- watch.py
+++ watch.py
@@ -2,14 +2,14 @@ import srv
 import cv2
 import connection
 from StreamFinishedException import StreamFinishedException
+import sys
 
 def getSources():
   c = connection.Connection("get sources")
   sources = c.getSources()
   return sources
 
-def run():
-  sources = getSources()
+def run(sources):
   connections = []
   for sourceName in sources:
     cv2.namedWindow(sourceName, cv2.WINDOW_NORMAL)
@@ -18,6 +18,13 @@ def run():
     if len(sources) == 0:
       break
     for i in range(len(sources)):
+      if sources[i] not in srvSources:
+        connections.pop(i)
+        src = sources.pop(i)
+        cv2.destroyWindow(src)
+        print "Error:", src, "not in SRV Sources"
+        print "SRV Sources:", getSources()
+        break
       try:
         cv2.imshow(sources[i], connections[i].getNextFrame())
       except StreamFinishedException:
@@ -31,4 +38,27 @@ def run():
   for sourceName in sources:
     cv2.destroyWindow(sourceName)
 
-run()
+def help():
+  print "To play specific sources:"
+  print "\tusage: python watch.py source_1 source_2 ..."
+  print "(indefinite amount of listed sources to be played)"
+  print "To play all sources:"
+  print "\tusage: python watch.py"
+
+srvSources = getSources()
+
+if len(sys.argv) == 1:
+  sources = getSources()
+elif len(sys.argv) == 2:
+  if sys.argv[1].lower() == "help":
+    help()
+  else:
+    #just 1 source
+    sources = [sys.argv[1]]
+else:
+  #each command line argument is a source name
+  sources = []
+  for i in range(1,len(sys.argv)):
+    sources.append(sys.argv[i])
+
+run(sources)
