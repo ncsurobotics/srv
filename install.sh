ed9a50cd8db6a2d0022b7b3884c86ef97be01768
Jacob Salzberg <jssalzbe@ncsu.edu>
2018-11-17 15:24:45 -0500

    Add shebang

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo mkdir /usr/local/lib/python2.7/dist-packages/srv
 sudo cp -r . /usr/local/lib/python2.7/dist-packages/srv
 sudo rm -f /usr/local/lib/python2.7/dist-packages/srv/*.sh
